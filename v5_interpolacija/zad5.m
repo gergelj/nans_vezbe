3eec5ea7357c37da1182507b24bd7a98648dd963
gergo <kgerg13@gmail.com>
2019-11-16 12:55:31 +0100

    vezbe 5

--- v5_interpolacija/zad5.m
+++ v5_interpolacija/zad5.m
@@ -1,17 +1,17 @@
-% Aproksimirati funkciju f(x) = x^2*sin(x) polinomom 3. stepena na intervalu [?pi, pi].
+% Aproksimirati funkciju f(x) = x^2*sin(x) polinomom 3. stepena na intervalu [-pi, pi].
 f = @(x) x.^2 .* sin(x);
 a = -pi;
 b = pi;
-% Odabrati i nacrtati potreban broj ta?aka funkcije f(x) za aproksimaciju polinomom 3. stepena.
-x = linspace(a, b, 3);
+% Odabrati i nacrtati potreban broj tacaka funkcije f(x) za aproksimaciju polinomom 3. stepena.
+x = linspace(a, b, 4);
 fX = f(x);
 scatter(x, fX); hold on
 
-% Lagran�ovom interpolacijom na?i polinom koj zadovoljava odabrane ta?ke i nacrtati ga.
+% Lagran�ovom interpolacijom naci polinom koj zadovoljava odabrane tacke i nacrtati ga.
 p = lagrangeInterp(x, fX);
 x = linspace(a, b, 100);
 
 % Nacrtati funkciju f(x) na intervalu.
 plot(x, f(x), 'blue');
-%plot(x, polyval(p, x), 'red');
+plot(x, polyval(p, x), 'red');
 legend('tacke', 'f(x)', 'polynomial'); hold off
--- v5_interpolacija/zad6.m
+++ v5_interpolacija/zad6.m
@@ -1,5 +1,5 @@
-% Lagranžovom interpolacijom odrediti parametre a0 , a1 , a2 i a3 tako da funkcija f(x) = a0 + a1*x + a2*x^2 + a3*x^3
-% zadovoljava slede?e:
+% Lagranzovom interpolacijom odrediti parametre a0 , a1 , a2 i a3 tako da funkcija f(x) = a0 + a1*x + a2*x^2 + a3*x^3
+% zadovoljava sledece:
 x = [0.1, 0.3, 0.6, 1.2];
 fX = [1.023, 1.261, 2.368, 9.064];
 
